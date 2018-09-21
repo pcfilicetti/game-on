5ffaf2a5d1f7649ee3ffeadbd6112fbe280e9502
Irene R <irene@smallwondermedia.com>
2018-09-20 23:00:57 -0400

    animated logo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+    function logoJourney() {
+        // logo image
+        var logo = $("<img>", {
+            src: "./assets/images/logo.png",
+            id: "logo",
+            alt: "logo image",
+            class: "logoImage slide-in-blurred-bottom"
+        });
+        $("#logoDiv").append(logo);
+
+        // var screenHeight = 
+
+        // start logo off screen
+        // $("#logo").animate({
+            // top: "200px",
+            // left: "100px",
+        
+
+        // })
+        // logo flies in to middle
+
+        // logo pauses
+        // logo flies up to top and stays there
+    };
+    
+    logoJourney();
 
 
 
