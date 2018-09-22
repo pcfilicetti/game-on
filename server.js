c083fc211d584d1136c9458b84f1bcaacb8d9bbc
Peter Filicetti <peterfilicetti@outlook.com>
2018-09-22 19:35:08 -0400

    fixed the server hosting

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use("/assets", express.static(__dirname + '/assets'));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
