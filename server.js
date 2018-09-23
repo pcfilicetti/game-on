d6eaa8d8d106780d43a351e0fb8c272649699378
Hubnick <jacoby.nick@gmail.com>
2018-09-23 03:54:34 -0400

    got a bunch of routes working

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
+
+// Dependencies
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+
+// Sets up the Express App
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use("/assets", express.static(__dirname + '/assets'));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+
+// Router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.use(express.static(path.join(__dirname, 'public'))); //Serves resources from public folder
+
+
+
+// Starts the server to begin listening
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
