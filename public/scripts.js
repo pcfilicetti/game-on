03f4684a02f076aa7a74b301572d63817f03ad99
PCFilicetti <peter.filicetti@kingsmantech.com>
2018-09-25 12:39:33 -0400

    clear the create value fields

--- public/scripts.js
+++ public/scripts.js
@@ -121,6 +121,12 @@ $(document).ready(function () {
                 image: image
             });
             window.alert("Event submitted!");
+            $("#title").val('');
+            $("#address").val('');
+            $("#zip").val('');
+            $("#email").val('');
+            $("#description").val('');
+            $("#datepicker").val('');
         }
     });
 
